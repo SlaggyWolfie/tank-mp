7ed259b0cce553e47895f819b31cdefabd98a50a
Slavi Stoev <sl_stoev@abv.bg>
2020-11-19 19:32:06 +0200

    Restarted project due to corruption

--- Source/tank_mp/tank_mp.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "tank_mp.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, tank_mp, "tank_mp" );
- 
